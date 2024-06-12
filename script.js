4a89f246df4d241165e52a9146ef0ccf436c808b
Lionel <lpei1900@gmail.com>
2024-06-12 17:00:11 -0400

    -Improved drawing function -Improved color picker -Made Background changer

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
-let row = 100;
-let column = 100;
+const DEFAULT_COLOR = 'black';
+const DEFAULT_SIZE = 32;
+const DEFAULT_MODE = 'color';
+const DEFAULT_BACKGROUND_COLOR = 'white';
+
+
 let isMouseDown = false;
-let color = "black";
+let drawingColor = DEFAULT_COLOR;
+let size = DEFAULT_SIZE;
+let backgroundColor = DEFAULT_BACKGROUND_COLOR;
+let mode = DEFAULT_MODE;
 
-let grid = document.querySelector("#grid");
+const grid = document.querySelector("#grid");
+const clearButton = document.querySelector("#clearButton");
+const colorPicker = document.querySelector("#colorPicker");
+const backgroundPicker = document.querySelector("#backgroundPicker");
 
-let clearButton = document.querySelector("#clearButton");
-clearButton.addEventListener("click", clear);
+clearButton.addEventListener("click", () => backgroundColorChange(backgroundColor));
+colorPicker.addEventListener("input", (e) => {colorChange(e.target.value)});
+backgroundPicker.addEventListener("input", (e) => {backgroundColorChange(e.target.value)});
 
-
-let colorPicker = document.querySelector("#colorPicker");
-colorPicker.addEventListener("input", colorChange);
-
-for(let i = 1; i <= row * column; i++){
-    const gridBox = document.createElement("div");
-    gridBox.setAttribute("class", "grid-box");
-    grid.appendChild(gridBox);
-    gridBox.addEventListener("mousedown", () => {isMouseDown = true;});
-    gridBox.addEventListener("mouseup", () => { isMouseDown = false; });
-    gridBox.addEventListener("mouseover", draw);
-}
-
+createGrid(DEFAULT_SIZE);
 const squares = document.querySelectorAll(".grid-box");
 
-function draw(e){
-    if(isMouseDown){
-        e.target.style.backgroundColor = color;
-    }
+function colorChange(newColor){
+  drawingColor = newColor;
 }
 
-function clear(){
-    squares.forEach(square => {square.style.backgroundColor = "white"});
+
+
+function backgroundColorChange(newbackgroundColor){
+  backgroundColor = newbackgroundColor;
+  setGridColor(newbackgroundColor);
 }
 
-function colorChange(){
-    color = colorPicker.value;
+function setGridColor(newbackgroundColor){
+  squares.forEach(square => {square.style.backgroundColor = newbackgroundColor});
 }
+
+
+function createGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridBox = document.createElement('div');
+    gridBox.classList.add('grid-box');
+    gridBox.addEventListener("mousedown", () => {isMouseDown = true;});
+    gridBox.addEventListener("mouseup", () => { isMouseDown = false; });
+    gridBox.addEventListener("mouseover", changeColor);
+    grid.appendChild(gridBox);
+  }
+}
+
+
+function changeColor(e) {
+  if(isMouseDown){
+    e.target.style.backgroundColor = drawingColor;
+  }
+}
+
+
